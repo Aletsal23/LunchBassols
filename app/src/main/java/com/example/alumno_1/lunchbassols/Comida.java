44ba19a853d32bbaa6b705b6d1335415c9e554f0
Gio <smooti55@outlook.com>
2019-04-09 10:48:21 -0500

    v3

--- app/src/main/java/com/example/alumno_1/lunchbassols/Comida.java
+++ app/src/main/java/com/example/alumno_1/lunchbassols/Comida.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 
 /**
@@ -18,6 +19,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Comida extends Fragment {
+
+    CheckBox chkChilaquiles;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
