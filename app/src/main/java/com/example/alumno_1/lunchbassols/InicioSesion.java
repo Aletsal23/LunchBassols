4583dc9795df0558ea1bcb96515ed2be9a0d711c
Giovanni <smooti55@outlook.com>
2019-04-15 10:59:32 -0500

    V5

--- app/src/main/java/com/example/alumno_1/lunchbassols/InicioSesion.java
+++ app/src/main/java/com/example/alumno_1/lunchbassols/InicioSesion.java
@@ -1,7 +1,9 @@
 package com.example.alumno_1.lunchbassols;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class InicioSesion extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class InicioSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+    }
+    public void entrar(View view)
+    {
+        Intent inicio=new Intent(this, MenuDrawer.class);
+        startActivity(inicio);
     }
 }
--- app/src/main/java/com/example/alumno_1/lunchbassols/MenuDrawer.java
+++ app/src/main/java/com/example/alumno_1/lunchbassols/MenuDrawer.java
@@ -1,18 +1,23 @@
 package com.example.alumno_1.lunchbassols;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 public class MenuDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -32,6 +37,7 @@ public class MenuDrawer extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,10 +86,12 @@ public class MenuDrawer extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager= getSupportFragmentManager();
 
-        if (id == R.id.nav_comida) {
-            Intent comida= new Intent(this,Comida.class);
-            startActivity(comida);
+        if (id == R.id.nav_comida)
+        {
+            fragmentManager.beginTransaction().replace(R.id.comida,new Comida()).commit();
+
         } else if (id == R.id.nav_bebidas) {
             Intent bebidas= new Intent(this,Bebidas.class);
             startActivity(bebidas);
