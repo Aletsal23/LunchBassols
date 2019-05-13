f3be61f63648a1120217660694168b0807b314e0
Alex23 <alex.alv230@gmail.com>
2019-05-13 10:58:44 -0500

    v21

--- app/src/main/java/com/example/alumno_1/lunchbassols/RegistroEmpleado.java
+++ app/src/main/java/com/example/alumno_1/lunchbassols/RegistroEmpleado.java
@@ -1,10 +1,12 @@
 package com.example.alumno_1.lunchbassols;
 
+import android.app.ProgressDialog;
 import  android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,8 +18,12 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -35,14 +41,16 @@ public class RegistroEmpleado extends AppCompatActivity implements View.OnClickL
     private RadioButton rdbCajero, rdbCocinero;
     private Button btnRegistrar;
     private String puesto;
-    private FirebaseFirestore mFirestore;
+    private FirebaseAuth mFireauth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_empleado);
 
-        mFirestore = FirebaseFirestore.getInstance();
+        mFireauth = FirebaseAuth.getInstance();
 
         btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
         txteNombre = (EditText) findViewById(R.id.txteNombre);
@@ -66,42 +74,44 @@ public class RegistroEmpleado extends AppCompatActivity implements View.OnClickL
             }
         });
 
+        progressDialog = new ProgressDialog(this);
+
         btnRegistrar.setOnClickListener(this);
     }
-    @Override
-    public void onClick(View v) {
-        String nombre = txteNombre.getText().toString();
-        String email = txteCorreoInicio.getText().toString();
-        String pass = pswContraInicio.getText().toString();
-        String dir = txteDireccion.getText().toString();
-        String edad = txtnEdad.getText().toString();
-        String tel = txtnTelefono.getText().toString();
-        Map<String, String> userMap = new HashMap<>();
 
-        userMap.put("nombre",nombre);
-        userMap.put("email", email);
-        userMap.put("password", pass);
-        userMap.put("direccion", dir);
-        userMap.put("edad", edad);
-        userMap.put("telefono", tel);
-        userMap.put("puesto", puesto);
+    private void registrar(){
+        String email = txteCorreoInicio.getText().toString().trim();
+        String pass = pswContraInicio.getText().toString().trim();
 
-        mFirestore.collection("Usuarios").add(userMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Se debe ingresar un correo",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(pass)){
+            Toast.makeText(this,"Se debe ingresar una contraseña",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        progressDialog.setMessage("Realizando registro en linea...");
+        progressDialog.show();
+
+        mFireauth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Toast.makeText(RegistroEmpleado.this, "Usuario creado", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
+            public void onComplete(@NonNull Task<AuthResult> task) {
 
-                String error = e.getMessage();
-
-                Toast.makeText(RegistroEmpleado.this,"Error : " + error, Toast.LENGTH_SHORT).show();
-
+                if(task.isSuccessful()){
+                    Toast.makeText(RegistroEmpleado.this,"Se ha registrado el correo",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RegistroEmpleado.this,"No se pudo registrar el correo",Toast.LENGTH_SHORT).show();
+                }
+         progressDialog.dismiss();
             }
         });
-        Intent inicio=new Intent(this, Inicio.class);
-        startActivity(inicio);
+    }
+
+    @Override
+    public void onClick(View v) {
+       registrar();
     }
 }
